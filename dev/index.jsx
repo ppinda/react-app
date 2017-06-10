2fa6f77baf4a83682f29945568ec0a535a1af6f8
peterp <ppinda@formcorp.com.au>
2017-06-10 13:07:59 +1000

    init commit to dev

---
+++ dev/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+var Buttonfy = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <button type={this.props.behaviorer}> {this.props.children}!</button>
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <div>
+        <Buttonfy behaviorer="submit">Submit</Buttonfy>
+        <Buttonfy behaviorer="reset">Reset</Buttonfy>
+        <Buttonfy behaviorer="submit">Submit</Buttonfy>
+    </div>,
+    document.querySelector("#container")
+);
+
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+/**
+ * Created by peterp on 27/05/17.
+ */
+
+var webpack = require("webpack");
+var path = require("path");
+
+var DEV = path.resolve(__dirname, "dev");
+var OUTPUT = path.resolve(__dirname, "output");
+
+var config = {
+    entry: DEV + "/index.jsx",
+    output: {
+        path: OUTPUT,
+        filename: "myCode.js"
+    },
+    module: {
+        loaders: [{
+            include: DEV,
+            loader: "babel-loader",
+        }]
+    }
+};
+
+module.exports = config;
